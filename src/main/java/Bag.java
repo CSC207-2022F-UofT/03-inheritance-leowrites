637d483264f8ce1ecc7d50e5d8f9279b065da2dd
leowrites <75044178+leowrites@users.noreply.github.com>
2022-09-16 22:25:33 -0400

    Completed TODOs

--- src/main/java/Bag.java
+++ src/main/java/Bag.java
@@ -5,6 +5,9 @@
  * 1. Introduction to Java helpful.
  */
 
+import java.sql.Array;
+import java.util.ArrayList;
+
 public abstract class Bag {
     /*
      * TODO: Create the following private instance variables
@@ -13,8 +16,10 @@ public abstract class Bag {
      *       - an int named capacity
      *       - an array of Strings named contents
      */
-
-
+    private String color;
+    private int numberOfContents;
+    private int capacity;
+    private ArrayList<String> contents;
 
 
     /*
@@ -27,6 +32,12 @@ public abstract class Bag {
      * its contents.)
      */
 
+    public Bag(String color, int capacity){
+        this.color = color;
+        this.capacity = capacity;
+        this.numberOfContents = 0;
+        this.contents = new ArrayList<String>();
+    }
 
 
 
@@ -37,7 +48,17 @@ public abstract class Bag {
      *           - getNumberOfContents
      *           - getCapacity
      */
+    public String getColor() {
+        return color;
+    }
 
+    public int getNumberOfContents() {
+        return numberOfContents;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
 
 
 
@@ -46,7 +67,9 @@ public abstract class Bag {
      *       color of this bag to the given color.
      */
 
-
+    public void setColor(String color){
+        this.color = color;
+    }
 
 
 
@@ -60,7 +83,14 @@ public abstract class Bag {
      *       This method should return true if the item was added
      *       and false otherwise.
      */
-
+    public boolean addItem(String item){
+        if (numberOfContents < capacity) {
+            this.contents.add(item);
+            this.numberOfContents += 1;
+            return true;
+        }
+        return false;
+    }
 
 
 
@@ -73,8 +103,17 @@ public abstract class Bag {
      *
      * If there are no items in this Bag, return null.
      *
-     * @return
+     * @return the last item in the bag.
      */
+    public String popItem(){
+        if (contents.size() < 1) return null;
+        else {
+            String item = contents.get(contents.size() - 1);
+            contents.remove(contents.size() - 1);
+            this.numberOfContents -= 1;
+            return item;
+        }
+    }
 
 
 
@@ -87,7 +126,7 @@ public abstract class Bag {
      */
     public void increaseCapacity(int n) {
         // TODO: Implement this method.
-
+        this.capacity += n;
     }
 
     /**
@@ -95,7 +134,7 @@ public abstract class Bag {
      * This method requires you to have created the private
      * instance variables mentioned above.
      *
-     * @return
+     * @return all the items in the bag
      */
     @Override
     public String toString() {
--- src/main/java/BagMain.java
+++ src/main/java/BagMain.java
@@ -10,12 +10,18 @@ class BagMain {
      * TODO: Implement this method
      * Enhance each of the bags in bags. If double_enhance_handbags is
      * True, then enhance any HandBags a second time.
-     *
+     * <p>
      * This method should work for *all* Bags! We will test this method
      * on new Bag types (and HandBag subclasses)!
      */
     public static void enhanceBags(Bag[] bags, boolean double_enhance_handbags) {
         // TODO: Implement this.
+        for (Bag bag : bags) {
+            if (double_enhance_handbags && bag instanceof HandBag) {
+                bag.enhance();
+            }
+            bag.enhance();
+        }
     }
 
     /**
@@ -29,5 +35,12 @@ class BagMain {
      */
     public static int countCrossbodyStraps(Bag[] bags) {
         // TODO: Implement this.
+        int totalStraps = 0;
+        for (Bag bag: bags) {
+            if (bag instanceof CrossbodyBag){
+                totalStraps += ((CrossbodyBag) bag).getNumberOfStraps();
+            }
+        }
+        return totalStraps;
     }
 }
